05ea137346e20d3a5a2342c830f954ea81ef8853
Orange_233 <874462262@qq.com>
2021-07-07 17:04:56 +0800

    ==OurOpenSource.Security== @ Fix `Rijndael` key length need fixed length, we use SHA256 to solve it.

--- OurOpenSource.Security/Cryptography/Rijndael.cs
+++ OurOpenSource.Security/Cryptography/Rijndael.cs
@@ -15,6 +15,11 @@ namespace OurOpenSource.Security.Cryptography
         //参考原文：https://www.cnblogs.com/liqipeng/archive/2013/03/23/4576174.html {
 
         /// <summary>
+        /// SHA256。
+        /// SHA256.
+        /// </summary>
+        private static SHA256 sha256 = SHA256.Create();
+        /// <summary>
         /// 默认密钥向量。
         /// </summary>
         private static byte[] d_vi = { 0x12, 0x34, 0x56, 0x78, 0x90, 0xAB, 0xCD, 0xEF, 0x12, 0x34, 0x56, 0x78, 0x90, 0xAB, 0xCD, 0xEF };
@@ -40,8 +45,9 @@ namespace OurOpenSource.Security.Cryptography
             //分组加密算法
             SymmetricAlgorithm des = System.Security.Cryptography.Rijndael.Create();
             //设置密钥及密钥向量
-            des.KeySize = pswdBytes.Length * 8;
-            des.Key = pswdBytes;
+            byte[] _pswd = ProcessPassword(pswdBytes);
+            des.KeySize = _pswd.Length * 8;
+            des.Key = _pswd;
             des.IV = d_vi;
             MemoryStream ms = new MemoryStream();
             CryptoStream cs = new CryptoStream(ms, des.CreateEncryptor(), CryptoStreamMode.Write);
@@ -51,6 +57,11 @@ namespace OurOpenSource.Security.Cryptography
             cs.Close();
             ms.Close();
             return cipherBytes;
+        }
+        private byte[] ProcessPassword(byte[] password)
+        {
+            //不管128、92，直接256
+            return sha256.ComputeHash(password);
         }
 
         /// <summary>
