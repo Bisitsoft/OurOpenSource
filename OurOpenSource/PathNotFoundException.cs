a8601fa4203868e832c79c539330428b0fb91a40
Orange_233 <874462262@qq.com>
2022-04-16 20:53:05 +0800

    从FreeloadMap引入FkPath类。（待修改

---
+++ OurOpenSource/PathNotFoundException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization;
+
+namespace OurOpenSource
+{
+    public class PathNotFoundException : IOException
+    {
+        public PathNotFoundException() : base() {; }
+        public PathNotFoundException(string message) : base(message) {; }
+        public PathNotFoundException(string message, Exception innerException) : base(message, innerException) {; }
+        protected PathNotFoundException(SerializationInfo info, StreamingContext context) : base(info, context) {; }
+    }
+}
